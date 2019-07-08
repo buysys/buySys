5a4f9a9eb208389633c757519a3102e5b8c7cc47
kyumin213 <yutong8023p@163.com>
2019-07-08 16:26:01 +0800

    心愿任务添加系统配置组件
    Signed-off-by: kyumin213 <yutong8023p@163.com>

--- src/pages/task/wishTask.vue
+++ src/pages/task/wishTask.vue
@@ -75,76 +75,10 @@
 		</el-dialog>
 		<!--系统配置-->
 		<el-dialog :title='buyNum' :visible.sync='systemConfigModal' :close-on-click-modal='false'>
-			<el-form class='viewMl50'>
-				<el-row>
-					<el-col :span='12' :xs='24'>
-						<el-form-item label='代理IP：'>
-							<span>112.0.23.2</span>
-						</el-form-item>
-					</el-col>
-					<el-col :span='12' :xs='24'>
-						<el-form-item label='内网IP：'>
-							<span>112.0.23.2</span>
-						</el-form-item>
-					</el-col>
-				</el-row>
-				<el-form-item label='默认网址：'>
-					<span>www.baidu.com</span>
-				</el-form-item>
-				<el-form-item label='买号ID：'>
-					<span>799b78de13f74c4295ed0acb98b96880</span>
-				</el-form-item>
-				<el-form-item label='登录账号：'>
-					<span>tamsindawson88@gmail.com</span>
-				</el-form-item>
-				<el-form-item label='登录密码：'>
-					<span>6880</span>
-				</el-form-item>
-				<el-form-item label='操作系统：'>
-					<span>Windows</span>
-				</el-form-item>
-				<el-form-item label='UserAgent：'>
-					<span>Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/530.4 (KHTML, like Gecko) Chrome/2.0.171.0 Safari/530.4</span>
-				</el-form-item>
-				<el-form-item label='浏览器版本号：'>
-					<span>5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/530.4 (KHTML, like Gecko) Chrome/2.0.171.0 Safari/530.4</span>
-				</el-form-item>
-				<el-form-item label='浏览器语言：'>
-					<span>EN</span>
-				</el-form-item>
-				<el-form-item label='浏览器插件：'>
-					<span>Chrome PDF Plugin,Chrome PDF Viewer,Native Client</span>
-				</el-form-item>
-				<el-form-item label='屏幕分辨率：'>
-					<span>1366*768</span>
-				</el-form-item>
-				<el-form-item label='CPU核心数：'>
-					<span>8</span>
-				</el-form-item>
-				<el-form-item label='系统字体数：'>
-					<span>311</span>
-				</el-form-item>
-				<el-form-item label='时区：'>
-					<span>UTC-05:00</span>
-				</el-form-item>
-				<el-row>
-					<el-col :span='8' :xs='24'>
-						<el-form-item label='屏幕颜色深度：'>
-							<span>323</span>
-						</el-form-item>
-					</el-col>
-					<el-col :span='8' :xs='24'>
-						<el-form-item label='授权账号：'>
-							<span>323</span>
-						</el-form-item>
-					</el-col>
-					<el-col :span='8' :xs='24'>
-						<el-form-item label='授权密码：'>
-							<span>323</span>
-						</el-form-item>
-					</el-col>
-				</el-row>
-			</el-form>
+			<systemConfig></systemConfig>
+			<div class="modelRight">
+				<el-button type='primary' @click='systemConfigModal=false'>关闭</el-button>
+			</div>
 		</el-dialog>
 		<!--查看任务详情-->
 		<el-dialog :title='orderTitle' :visible.sync="viewTaskDateilsModel" width="60%" :close-on-click-modal="false">
@@ -307,6 +241,7 @@
 </template>
 
 <script>
+	import systemConfig from '../../common/systemConfig'
 	import buyNum from '../../common/buyNum'
 	export default {
 		name: 'wishTask',
@@ -349,7 +284,7 @@
 			}
 		},
 		components:{
-			buyNum
+			buyNum,systemConfig
 		},
 		created() {
 			this.getAllData()
