1f7353141d10fc15abd80d20f7153b55942848bf
EnwjingW <619944680@qq.com>
2019-07-17 14:46:21 +0800

    订单管理添加分页的方法

--- src/pages/order/orderList.vue
+++ src/pages/order/orderList.vue
@@ -485,7 +485,14 @@
 						}
 					}
 				}
-			}
+			},
+      //分页
+      handleSizeChange(val) {
+      	console.log(`每页 ${val} 条`)
+      },
+      handleCurrentChange(val) {
+      	console.log(`当前页: ${val}`)
+      }
 		}
 	}
 </script>
