769522b2d173d9b2c6207884ff1424cc49c61a4e
YHCorange <635000035@qq.com>
2019-09-02 17:03:11 +0800

    新增加销量预估模块
    Signed-off-by: YHCorange <635000035@qq.com>

---
+++ src/pages/ParameterConfig/salesEstimate.vue
@@ -0,0 +1,255 @@
+<template>
+	<div>
+		<el-collapse-transition>
+			<div class="searchBox mb20">
+				<el-form ref="searchForm" :model="searchForm" class="form-item" label-width="80px">
+					<el-row>
+						<el-col :xs="24" :span="6">
+							<el-form-item label="产品排名">
+								<el-input v-model="searchForm.searchkeywords" placeholder="请输入产品排名进行查询" class="disInline"></el-input>
+							</el-form-item>
+						</el-col>
+						<el-col :xs="24" :span="4" class="ml20">
+							<el-button type="primary" size="medium">查询</el-button>
+							<el-button size="medium" @click="resetSearch">重置</el-button>
+						</el-col>
+					</el-row>
+				</el-form>
+			</div>
+		</el-collapse-transition>
+		<div class="mb20">
+			<el-button type="success" size="medium" @click="addModelShow"><i class="el-icon-plus"></i>新增</el-button>
+			<el-button type="primary" size="medium" @click="editModelShow" :disabled="editDisabled"><i class="el-icon-edit-outline"></i>修改</el-button>
+      <el-button type="danger" size="medium" @click="delData" :disabled="delDisabled"><i class="el-icon-delete"></i>删除</el-button>
+		</div>
+		<div class="mt10">
+			<el-table :data="tableData" id="exportData" style="width: 100%" :header-cell-style="{background:'#fafafa'}" @selection-change="handleSelectionChange">
+				<el-table-column type="selection"></el-table-column>
+				<el-table-column prop="CountryId" label="国家" align="center"></el-table-column>
+				<el-table-column prop="ProductByASIN" label="产品类型" align="center"></el-table-column>
+				<el-table-column prop="OrderNumber" label="排名" align="center"></el-table-column>
+        <el-table-column prop="OrderNumber" label="预估销量" align="center"></el-table-column>
+			</el-table>
+			<div class="mt30">
+				<el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage" :page-sizes="[100, 200, 300, 500]" :page-size="10" layout="total, sizes, prev, pager, next, jumper" :total="total">
+				</el-pagination>
+			</div>
+		</div>
+		<!-- 新增修改 -->
+		<el-dialog :title="title" :visible.sync="editModel" :close-on-click-modal="false">
+			<el-form :model="editForm" :rules="editRules" label-width="80px" status-icon>
+				<el-form-item label="国家">
+				  <el-select v-model="editForm.country" placeholder="请选择国家" @change="getType()" style="width: 100%;">
+				    <el-option v-for="item in country_data" :key="item" :label="item" :value="item"></el-option>
+				  </el-select>
+				</el-form-item>
+				<el-form-item label="产品类型">
+					<el-select v-model="editForm.type" placeholder="请选择产品类型" style="width: 100%;">
+					  <el-option v-for="item in type_data_active" :key="item" :label="item" :value="item"></el-option>
+					</el-select>
+				</el-form-item>
+				<el-form-item label="排名">
+					<el-input v-model="editForm.rank" placeholder="请输入排名"></el-input>
+				</el-form-item>
+				<el-form-item label="预估销量">
+					<el-input v-model="editForm.num" placeholder="请输入预估销量"></el-input>
+				</el-form-item>
+			</el-form>
+			<div slot="footer" class="dialog-footer">
+				<el-button type="primary" @click="editModel=false">确 定</el-button>
+				<el-button @click="editModel=false">取 消</el-button>
+			</div>
+		</el-dialog>
+    <!-- 删除-->
+    <el-dialog title="温馨提示" :visible.sync="delModel" :close-on-click-modal="false" center="" width="30%">
+    	<div class="del-dialog-cnt textCen">确认要删除该数据吗？</div>
+    	<span slot="footer" class="dialog-footer">
+        <el-button type="primary" size="medium">是</el-button>
+        <el-button @click="delModel=false" size="medium">否</el-button>
+      </span>
+    </el-dialog>
+	</div>
+</template>
+
+<script>
+	import FileSaver from 'file-saver'
+	import XLSX from 'xlsx'
+	export default {
+		name: 'autoShare',
+		data() {
+			return {
+        country_data:['United States of America','United Kingdom','Spain','Mexico','Italy','India','Germany','France','Canada'],
+        type_data:[
+          {country:'United States of America',type:['Appliances','Arts, Crafts & Sewing','Automotive','Baby','Beauty','Beauty & Personal Care','Books','Camera & Photo','Cell Phones & Accessories','Clothing, Shoes & Jewelry','Computers & Accessories','Electronics','Grocery & Gourmet Food','Health & Household','Home & Garden','Home & Kitchen','Home Improvement','Industrial & Scientific','Kindle Store','Kitchen & Dining','Music','Musical Instruments','Office Products','Patio, Lawn & Garden','Pet Supplies','Software','Sports & Outdoors','Toys & Games','Video Games','Watches']},
+          {country:'United Kingdom',type:['Baby','Beauty','Car & Motorbike','Clothing','Computers','DIY & Tools','Electronics','Garden & Outdoors','Grocery','Health & Personal Care','Jewellery','Kitchen & Home','Large Appliances','Lighting','Luggage','Musical Instruments','Office Products','PC & Video Games','Pet Supplies','Shoes & Bags','Software','Sports & Outdoors','Toys & Games','Watches']},
+          {country:'Spain',type:['Bebé','Belleza','Bricolaje y herramientas','Coche y moto','Deportes y aire libre','Electrónica','Equipaje','Hogar','Iluminación','Industria, empresas y ciencia','Informática','Instrumentos musicales','Jardín','Joyería','Juguetes y juegos','Libros','Oficina y papelería','Películas y TV','Relojes','Ropa y accesorios','Salud y cuidado personal','Software','Supermercado','Videojuegos','Zapatos y complementos']},
+          {country:'Mexico',type:['Bebé','Deportes y Aire Libre','Electrónicos','Herramientas y Mejoras del Hogar','Hogar y Cocina','Industria, Empresas y Ciencia','Juegos y juguetes','Libros','Mascotas','Música','Oficina y Papelería','Ropa, Zapatos y Accesorios','Salud, Belleza y Cuidado Personal','Software','Videojuegos']},
+          {country:'Italy',type:['Abbigliamento','Alimentari e cura della casa','Auto e Moto','Bellezza','Casa e cucina','CD e Vinili','Commercio, Industria e Scienza','Elettronica','Fai da te','Film e TV','Giardino e giardinaggio','Giochi e giocattoli','Gioielli','Illuminazione','Informatica','Libri','Orologi','Prima infanzia','Salute e cura della persona','Scarpe e borse','Software','Sport e tempo libero','Strumenti musicali e DJ','Valigeria','Videogiochi']},
+          {country:'India',type:['Baby','Bags, Wallets & Luggage','Beauty','Books','Car & Motorbike','Clothing & Accessories','Electronics','Gift Cards','Grocery & Gourmet Foods','Health & Personal Care','Home & Kitchen','Industrial & Scientific','Jewellery','Movies & TV Shows','Music','Musical Instruments','Office Products','Pet Supplies','Shoes & Handbags','Software','Sports, Fitness & Outdoors','Toys & Games','Video Games','Watches']},
+          {country:'Germany',type:['Auto','Baby','Baumarkt','Beauty','Bekleidung','Beleuchtung','Bücher','Bürobedarf & Schreibwaren','Computer & Zubehör','Drogerie & Körperpflege','DVD & Blu-ray','Elektro-Großgeräte','Elektronik','Fremdsprachige Bücher','Games','Garten','Gewerbe, Industrie & Wissenschaft','Haustier','Kamera','Koffer, Rucksäcke & Taschen','Küche & Haushalt','Lebensmittel & Getränke','Motorrad','Musikinstrumente','Schmuck','Schuhe & Handtaschen','Software','Spielzeug','Sport & Freizeit','Uhren']},
+          {country:'France',type:['Animalerie','Auto et Moto','Bagages','Beauté et Parfum','Bébés & Puériculture','Bijoux','Bricolage','Chaussures et Sacs','Commerce, Industrie & Science','Cuisine & Maison','DVD & Blu-ray','Epicerie','Fournitures de bureau','Gros électroménager','High-tech','Hygiène et Soins du corps','Informatique','Instruments de musique','Jardin','Jeux et Jouets','Jeux vidéo','Livres','Logiciels','Luminaires et Eclairage','Montres','Sports et Loisirs','Vêtements et accessoires']},
+          {country:'Canada',type:['Automotive','Baby','Beauty','Books','Clothing & Accessories','Computer & Video Games','DVD','Electronics','Gift Cards','Grocery & Gourmet Food','Health & Personal Care','Home & Kitchen','Industrial & Scientific','Jewelry','Luggage & Bags','Music','Musical Instruments, Stage & Studio','Office Products','Patio, Lawn & Garden','Pet Supplies','Shoes & Handbags','Software','Sports & Outdoors','Tools & Home Improvement','Toys & Games','Video','Watches']}
+          ],
+        type_data_active:'',
+				loading: true,
+				viewModel: false,
+				editModel: false,
+				drModel: false,
+				editDisabled: true,
+				delDisabled: true,
+        delModel: false,
+				tableData: [{
+						"Numbers": "20190605105636229596",
+						"Picture": "",
+						"CountryId": "United States of America",
+						"Forum": "Amazon",
+						"ProductByASIN": "777888999a",
+						"ProductPrice": 15.99,
+						"ServiceType": "不留评",
+						"OrderNote": "待付款",
+						"Status": "1",
+						"OrderNumber": 1314520,
+						"OrderTime": "2019-02-03T00:00:00",
+						"Remark": ""
+					},
+					{
+						"Numbers": "20190611174157617041",
+						"Picture": "",
+						"CountryId": "United Kingdom",
+						"Forum": "Amazon",
+						"ProductByASIN": "B07P6KVGF8",
+						"ProductPrice": 18.99,
+						"ServiceType": "不留评",
+						"OrderNote": "待确认",
+						"Status": "0",
+						"OrderNumber": 7758258,
+						"OrderTime": "2019-04-02T00:00:00",
+						"Remark": ""
+					}
+				],
+				checkBoxData: [],
+				title: '',
+				allNum: 0,
+				active: 1,
+				currentPage: 1,
+				pageSize: '0',
+				total: 100,
+				searchForm: {
+					platform: '全部',
+					searchkeywords: ''
+				},
+				editForm: {
+					country: '',
+					type: '',
+					rank: '',
+					num: ''
+				},
+				editRules: {
+					country: [{
+						required: true,
+						message: '请选择国家',
+						trigger: 'blur'
+					}],
+					type: [{
+						required: true,
+						message: '请选择产品类型',
+						trigger: 'blur'
+					}],
+					rank: [{
+						required: true,
+						message: '请输入排名',
+						trigger: 'blur'
+					}],
+          num: [{
+          	required: true,
+          	message: '请输入预估销量',
+          	trigger: 'blur'
+          }]
+				},
+			}
+		},
+		created() {
+//			this.getAllData()
+		},
+		methods: {
+      //根据国家获取产品类型
+      getType(){
+        let _this = this
+        let country = _this.editForm.country
+        for( let item in _this.type_data){
+          if(_this.type_data[item].country==country){
+            _this.type_data_active = _this.type_data[item].type
+            _this.editForm.type = _this.type_data[item].type[0]
+          }
+        }
+      },
+			//获取数据
+			getAllData() {
+				let _this = this
+				_this.active = 1
+				_this.axios.get(_this.GLOBAL.BASE_URL + 'api/OrderManagement/AddOrderByType').then((res) => {
+					_this.tableData = res.data.data
+					_this.allNum = res.data.data.length
+					_this.loading = false
+				}).catch((error) => {
+					console.log(error)
+				})
+			},
+			// 重置
+			resetSearch() {
+				let _this = this
+				_this.searchForm = {
+					platform: '全部',
+					searchkeywords: ''
+				}
+			},
+			// 是否有选中
+			handleSelectionChange(val) {
+				let _this = this
+				_this.checkBoxData = val
+				let checkNum = _this.checkBoxData.length
+				if(checkNum == 1) {
+					_this.editDisabled = false
+					_this.delDisabled = false
+				} else if(checkNum > 1) {
+					_this.editDisabled = true
+					_this.delDisabled = false
+				} else {
+					_this.editDisabled = true
+					_this.delDisabled = true
+				}
+			},
+			// 新增
+			addModelShow() {
+				let _this = this
+				_this.editModel = true
+				_this.title = '预估销量添加'
+			},
+			// 修改
+			editModelShow() {
+				let _this = this
+				_this.editModel = true
+				let item = _this.checkBoxData[0]
+				let country = item.CountryId
+				_this.title = country + ' 预估销量修改'
+				_this.editForm.country = item.CountryId;
+			},
+      // 删除
+      delData() {
+      	let _this = this
+      	_this.delModel = true
+      },
+			//分页
+			handleSizeChange(val) {
+				console.log(`每页 ${val} 条`)
+			},
+			handleCurrentChange(val) {
+				console.log(`当前页: ${val}`)
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
